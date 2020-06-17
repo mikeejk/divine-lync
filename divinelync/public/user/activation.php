fb0851a6e0cca533ad8508ae53bd504a82d585a7
webttp <smartin.black@gmail.com>
2020-06-17 07:49:38 +0530

    create mysql db
    asd

--- divinelync/public/user/activation.php
+++ divinelync/public/user/activation.php
@@ -1,37 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-include("../config/Class.Crud.Php");
-
-if(!empty($_GET['code']) && isset($_GET['code']))
-{
-	
-	$code=$_GET['code'];
-	$tablename="user";
-	$InsColumnVal = array("activation_key"=>$code,"status"=>0);
-	$fetch=$obj->fetch($tablename, $InsColumnVal);
-	if(count($fetch) > 0)
-	{
-		$fetch=$obj->fetch($tablename, $InsColumnVal);
-		if(count($fetch)=="1")
-		{
-			$user_id=$fetch['0']['user_id'];
-			$set = array("status"=>1);
-			$condition = array("user_id"=>$user_id,"status"=>'0');
-			$msg=$obj->update($tablename, $set,$condition);
-			if($msg=="Record updated successfully")
-			{
-				http("location:")
-			}
-		}
-		else
-		{
-			$msg ="Your account is already active, no need to activate again";
-		}
-	}
-	else
-	{
-		$msg ="Wrong activation code.";
-	}
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-?>
 
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
